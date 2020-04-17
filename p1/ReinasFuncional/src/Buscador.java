b11dd4fd563703bbdf6130b51922d87c892d401c
Victor Peralta <victorperalta.correo@gmail.com>
2020-04-17 23:25:51 +0200

    clases Buscar y Celda importadas de versión imperativa

---
+++ p1/ReinasFuncional/src/Buscador.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Buscador {
+    private final int dimension;
+
+    Buscador(int dimension){
+        this.dimension = dimension;
+    }
+
+    public ArrayList<Tablero> resolver(){
+        return ubicarReina(dimension-1);
+    }
+
+    public ArrayList<Tablero> ubicarReina(int fila){
+        ArrayList<Tablero> tableros = new ArrayList<>();
+
+        if(fila == -1){
+            Tablero tablero = new Tablero(dimension);
+            tableros.add(tablero);
+        }
+        else{
+            ArrayList<Tablero> soluciones = new ArrayList<>();
+            tableros = ubicarReina(fila-1);
+
+            for (Tablero tablero : tableros) {
+                for (int j = 0; j < dimension; j++) {
+                    Celda c1 = new Celda(fila, j);
+                    if (tablero.posicionSegura(c1)) {
+                        Tablero t = new Tablero(tablero);
+                        t.ponerReina(fila, j);
+                        soluciones.add(t);
+                    }
+                }
+            }
+
+            tableros = soluciones;
+        }
+
+        return tableros;
+    }
+}
---
+++ p1/ReinasFuncional/src/Celda.java
@@ -0,0 +1,25 @@
+import static java.lang.Math.abs;
+
+public class Celda {
+    private int fila;
+    private int columna;
+
+    Celda(int fila, int columna){
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public static boolean conflicto(Celda c1, Celda c2){
+        if(c1.getFila() == c2.getFila() || c1.getColumna() == c2.getColumna())
+            return true;
+        else return abs(c1.getFila() - c2.getFila()) == abs(c1.getColumna() - c2.getColumna());
+    }
+}

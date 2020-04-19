1168b4b6b4176b94490c40e9a1e83dc844e10c5b
Victor Peralta <victorperalta.correo@gmail.com>
2020-04-19 02:51:29 +0200

    p1 terminada

--- p1/ViajanteComercio/src/ViajanteComercio.java
+++ p1/ViajanteComercio/src/ViajanteComercio.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class ViajanteComercio {
     public static void main (String [] args){
-        Problema problema = new Problema("berlin52.tsp");
+        Problema problema = new Problema("test6.tsp");
 
         VecinoMasCercano vmc = new VecinoMasCercano();
         Ruta ruta = vmc.calcularRutaOptima(problema);

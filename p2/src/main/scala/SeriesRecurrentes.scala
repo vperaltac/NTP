e081256845baba0cef602151965bc48865230263
Victor Peralta <victorperalta.correo@gmail.com>
2020-05-10 01:59:04 +0200

    alternativa interesante añadida

--- p2/src/main/scala/SeriesRecurrentes.scala
+++ p2/src/main/scala/SeriesRecurrentes.scala
@@ -5,6 +5,21 @@ object SeriesRecurrentes {
         else fibonacci(n-1, l1, l0+l1)
     }
 
+    // NOTE: alternativa muy valida para ahorrar duplicado de codigo
+    // ATENCION!!! TENER EN CUENTA
+    // IMPORTANTE!!
+    // REPASAR!
+    @scala.annotation.tailrec
+    def fibb(n:Int, l0:Int, l1:Int, op : (Int,Int) => Int) : Int = {
+        if(n == 0) l0
+        else fibb(n-1, l1,op(l0,l1),op)
+    }
+
+
+    def suma(v1: Int, v2: Int) : Int = {
+        v1 + v2
+    }
+
     @scala.annotation.tailrec
     def lucas(n:Int, l0:Int, l1:Int) : Int = {
         if(n == 0) l0
@@ -43,5 +58,9 @@ object SeriesRecurrentes {
         println("Pell-Lucas de 8: " + serie(2,2,8,pell))
 
         // Serie de Jacobsthal
-        println("Jacobsthal de 8: " + serie(0,1,8,jacobsthal))    }
+        println("Jacobsthal de 8: " + serie(0,1,8,jacobsthal))
+
+        // TEST FIBB
+        println(fibb(8,0,1,suma))
+    }
 }

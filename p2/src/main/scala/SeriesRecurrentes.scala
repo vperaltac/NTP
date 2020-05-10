c23bbfb70bb932794db176bf34f2077cfb5ef7e5
Victor Peralta <victorperalta.correo@gmail.com>
2020-05-10 02:06:56 +0200

    esta solucion es mucho mejor

--- p2/src/main/scala/SeriesRecurrentes.scala
+++ p2/src/main/scala/SeriesRecurrentes.scala
@@ -1,66 +1,41 @@
 object SeriesRecurrentes {
+    // función general para series recurrentes
     @scala.annotation.tailrec
-    def fibonacci(n:Int, l0:Int, l1:Int) : Int = {
+    def serie(n:Int, l0:Int, l1:Int, op : (Int,Int) => Int) : Int = {
         if(n == 0) l0
-        else fibonacci(n-1, l1, l0+l1)
+        else serie(n-1, l1,op(l0,l1),op)
     }
 
-    // NOTE: alternativa muy valida para ahorrar duplicado de codigo
-    // ATENCION!!! TENER EN CUENTA
-    // IMPORTANTE!!
-    // REPASAR!
-    @scala.annotation.tailrec
-    def fibb(n:Int, l0:Int, l1:Int, op : (Int,Int) => Int) : Int = {
-        if(n == 0) l0
-        else fibb(n-1, l1,op(l0,l1),op)
-    }
-
-
+    // Fibonacci y Lucas utilizan suma
     def suma(v1: Int, v2: Int) : Int = {
         v1 + v2
     }
 
-    @scala.annotation.tailrec
-    def lucas(n:Int, l0:Int, l1:Int) : Int = {
-        if(n == 0) l0
-        else lucas(n-1, l1, l0+l1)
+    // utilizada por Pell y Pell-Lucas
+    def operacion1(v1: Int, v2: Int) : Int = {
+        2*v1 + v2
     }
 
-    @scala.annotation.tailrec
-    def pell(n:Int, l0:Int, l1:Int) : Int = {
-        if(n == 0) l0
-        else pell(n-1, l1,(2*l0)+l1)
-    }
-
-    @scala.annotation.tailrec
-    def jacobsthal(n:Int, l0:Int, l1:Int) : Int = {
-        if(n == 0) l0
-        else jacobsthal(n-1, l1,l0+(2*l1))
-    }
-
-
-    def serie(l0: Int, l1: Int, n: Int, operacion : (Int,Int,Int) => Int): Int ={
-        operacion(n,l0,l1)
+    // utilizada por Jacobsthal
+    def operacion2(v1: Int, v2: Int) : Int = {
+        v1 + 2*v2
     }
 
     def main(args: Array[String]): Unit ={
         println(".......................Series Recurrentes.......................")
         // Serie de Fibonacci
-        println("Fibonacci de 8: " + serie(0,1,8,fibonacci))
+        println("Fibonacci de 8: " + serie(8,0,1,suma))
 
         // Serie de Lucas
-        println("Lucas de 8: " + serie(2,1,8,lucas))
+        println("Lucas de 8: " + serie(8,2,1,suma))
 
         // Serie de Pell
-        println("Pell de 8: " + serie(2,6,8,pell))
+        println("Pell de 8: " + serie(8,2,6,operacion1))
 
         // Serie de Pell-Lucas
-        println("Pell-Lucas de 8: " + serie(2,2,8,pell))
+        println("Pell-Lucas de 8: " + serie(8,2,2,operacion1))
 
         // Serie de Jacobsthal
-        println("Jacobsthal de 8: " + serie(0,1,8,jacobsthal))
-
-        // TEST FIBB
-        println(fibb(8,0,1,suma))
+        println("Jacobsthal de 8: " + serie(8,0,1,operacion2))
     }
 }

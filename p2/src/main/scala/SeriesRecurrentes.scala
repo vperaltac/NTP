86ced999adef81b6e8a4aecb4a98aad7a794bd98
Victor Peralta <victorperalta.correo@gmail.com>
2020-05-10 13:23:21 +0200

    Series recurrentes comentadas

--- p2/src/main/scala/SeriesRecurrentes.scala
+++ p2/src/main/scala/SeriesRecurrentes.scala
@@ -1,22 +1,39 @@
 object SeriesRecurrentes {
-    // función general para series recurrentes
+    //
+    /**
+     * función general para series recurrentes.
+     * Se utilizará la función op para calcular los valores de la serie
+     *
+     * @param n  Posición de la serie a calcular
+     * @param l0 Primer valor de la serie
+     * @param l1 Segundo valor de la serie
+     * @param op Operación para obtener el valor en la posición n
+     * @return
+     */
     @scala.annotation.tailrec
     def serie(n:Int, l0:Int, l1:Int, op : (Int,Int) => Int) : Int = {
         if(n == 0) l0
         else serie(n-1, l1,op(l0,l1),op)
     }
 
-    // Fibonacci y Lucas utilizan suma
+    /*
+     * Operación suma para Fibonacci y Lucas
+     */
     def suma(v1: Int, v2: Int) : Int = {
         v1 + v2
     }
 
     // utilizada por Pell y Pell-Lucas
+    /*
+     * 2*Li-1 + Li-2 para Pell y Pell-Lucas
+     */
     def operacion1(v1: Int, v2: Int) : Int = {
         2*v1 + v2
     }
 
-    // utilizada por Jacobsthal
+    /**
+     * Li-1 + 2*Li-2 para Jabobsthal
+     */
     def operacion2(v1: Int, v2: Int) : Int = {
         v1 + 2*v2
     }

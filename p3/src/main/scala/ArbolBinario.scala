61160fe369b2083dedaab35924d9561deeca7c4d
Victor Peralta <victorperalta.correo@gmail.com>
2020-06-27 12:51:52 +0200

    ahora cada ArbolBinario tiene un id único. closes #7

--- p3/src/main/scala/ArbolBinario.scala
+++ p3/src/main/scala/ArbolBinario.scala
@@ -1,4 +1,11 @@
+object ArbolBinario{
+    private var cont = 0
+    private def id = {cont += 1; cont}
+}
+
 trait ArbolBinario{
+    val id: Int = ArbolBinario.id
+
     def preorden(): List[Any]
     def postorden(): List[Any]
     def tamArbol(): Int
